9f4419907e5550483eb7090ed3443f7cf932df16
Shubhamovale <shubhamovale12@gmail.com>
2026-03-21 10:36:13 +0530

    Update model version in generate_content.py

--- generate_content.py
+++ generate_content.py
@@ -23,7 +23,7 @@ Return ONLY JSON no extra text:
 }}"""
 
     response = client.models.generate_content(
-        model="gemini-2.0-flash",
+        model="gemini-1.5-flash-latest",
         contents=prompt
     )
 
